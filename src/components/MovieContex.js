c68997427797407e95f48c38bd9a08f337ba99bb
Haekal Budiman <hbudiman45@gmail.com>
2019-06-23 13:40:21 +0700

    Add Wall_e

--- src/components/MovieContex.js
+++ src/components/MovieContex.js
@@ -6,7 +6,8 @@ export const MovieProvider = props => {
   const [movies, setMovies] = useState([
     { name: "Inception", price: "$10", id: 1 },
     { name: "Forest Gum", price: "$10", id: 2 },
-    { name: "Shawshank Redemption", price: "$10", id: 3 }
+    { name: "Shawshank Redemption", price: "$10", id: 3 },
+    { name: "Wall-E", price: "$10", id: 5 }
   ]);
   return (
     <MovieContex.Provider value={[movies, setMovies]}>
